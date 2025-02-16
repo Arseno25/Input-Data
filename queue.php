49fcdd5c3f35440ed51b7a4054965d8a8dfe6b82
Arseno25 <utomnestagumara@gmail.com>
2025-02-17 00:59:33 +0700

    ✨ (queue.php): introduce new queue.php file to run 'php artisan queue:work --stop-when-empty' command in the background and handle the output and status of the command execution

---
+++ queue.php
@@ -0,0 +1,16 @@
+<?php
+
+$artisanPath = __DIR__ . '/artisan';
+
+if (!file_exists($artisanPath)) {
+    die("File 'artisan' tidak ditemukan di path: $artisanPath\n");
+}
+
+exec("php $artisanPath queue:work --stop-when-empty", $queueOutput, $queueStatus);
+
+if ($queueStatus === 0) {
+    echo "Command 'php artisan queue:work --stop-when-empty' berhasil dijalankan di background.\n";
+} else {
+    echo "Command gagal dijalankan dengan status: $queueStatus\n";
+    print_r($queueOutput);
+}
