2ce1c293db93fc4911e9323be5c98dc026799a10
utomnestagumara <utomnestagumara@gmail.com>
2025-02-25 12:32:24 +0000

    fix error database connection

---
+++ database/migrations/2025_02_25_192959_change_data_column_type.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Config::get('database.default') === 'pgsql') {
+            Schema::table('notifications', function (Blueprint $table) {
+                $table->jsonb('data')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Config::get('database.default') === 'pgsql') {
+            Schema::table('notifications', function (Blueprint $table) {
+                $table->text('data')->change();
+            });
+        }
+    }
+};
