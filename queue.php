f106d46203477792ba128ccf7a425ffc72f47718
Arseno25 <utomnestagumara@gmail.com>
2025-02-27 22:35:44 +0700

    🔧 (queue.php): update queue command options to improve queue processing efficiency and reliability

--- queue.php
+++ queue.php
@@ -6,10 +6,10 @@ if (!file_exists($artisanPath)) {
     die("File 'artisan' tidak ditemukan di path: $artisanPath\n");
 }
 
-exec("php $artisanPath queue:work --stop-when-empty", $queueOutput, $queueStatus);
+exec("php $artisanPath queue:work --sleep=3 --tries=1 --timeout=60 --backoff=0", $queueOutput, $queueStatus);
 
 if ($queueStatus === 0) {
-    echo "Command 'php artisan queue:work --stop-when-empty' berhasil dijalankan di background.\n";
+    echo "Command 'php artisan queue:work --sleep=3 --tries=1 --timeout=60 --backoff=0' berhasil dijalankan di background.\n";
 } else {
     echo "Command gagal dijalankan dengan status: $queueStatus\n";
     print_r($queueOutput);
