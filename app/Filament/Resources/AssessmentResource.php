47ee80aa4e2dda56d9be5342065fd4834526c517
Arseno25 <utomnestagumara@gmail.com>
2025-06-24 21:25:20 +0700

    ✨ (AssessmentResource.php): enable localized action labels and disable adding in form to support i18n and prevent unauthorized additions

--- app/Filament/Resources/AssessmentResource.php
+++ app/Filament/Resources/AssessmentResource.php
@@ -215,16 +215,17 @@ class AssessmentResource extends Resource
                                 return 'Score';
                             })
                             ->columnSpanFull()
-                    // ->addActionLabel(function () {
-                    //     $locale = app()->getLocale();
+                    ->addActionLabel(function () {
+                        $locale = app()->getLocale();
 
-                    //     if ($locale == 'id') {
-                    //         return 'Tambah Nilai';
-                    //     }
+                    if ($locale == 'id') {
+                        return 'Tambah Nilai';
+                    }
 
-                    //     return 'Add Score';
-                    // })
-                    ->columnSpanFull(),
+                        return 'Add Score';
+                    })
+                    ->columnSpanFull()
+                    ->addable(false),
                     ]),
             ]);
     }
